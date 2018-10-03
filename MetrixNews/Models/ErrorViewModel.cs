51e48bcf63d515697a7fade3cf312f81b3a4cd29
Paden <paden19@hotmail.com>
2018-10-02 20:05:31 -0600

    Add project files.

---
+++ MetrixNews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MetrixNews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
