9c0c69177d2ed376bcdde1660fd0e4d610ba4e04
Paden <paden19@hotmail.com>
2018-10-06 19:51:12 -0600

    Built "First time visitor popup", Fixed sliders, and added arrow controls.

--- MetrixNews/wwwroot/js/site.js
+++ MetrixNews/wwwroot/js/site.js
@@ -15,6 +15,7 @@ var MetrixNews = {
                 success: function (result) {
                     $('.mainContent').html(result);
                     MetrixNews.InitializeArticleSliders();
+                    MetrixNews.CheckIsFirstVisit();
                 },
                 error: function (xhr) {
                     alert('Error: ' + xhr.statusText);
@@ -26,16 +27,52 @@ var MetrixNews = {
         var mainContent = $('.mainContent');
 
         mainContent.find('.slider').slick({
-            infinite: true,
+            /*infinite: true,
             slidesToShow: 5,
-            slidesToScroll: 5
+            slidesToScroll: 5*/
+            centerMode: true,
+            centerPadding: '40%',
+            slidesToShow: 1
         });
+
+        MetrixNews.ResizeSliders();
     },
     InitializeStatusSliders: function () {
 
+    },
+    CheckIsFirstVisit: function () {
+        var popup = $('.modalPopupContainer');
+        //popup.show();
+        $('.closeModal').off('click').on('click', function () {
+            popup.hide();
+        });
+    },
+    ResizeSliders: function () {
+        var mainContent = $('.mainContent');
+        var screenWidth = mainContent.width();
+
+        var cardSize = 240;
+        var cardMargin = 7 * 2;
+        var categoryMargin = 50 * 2;
+
+        var newPadding = (screenWidth - (cardSize + cardMargin + categoryMargin)) / 2;
+
+        if (newPadding < 0) {
+            newPadding = 0;
+        }
+
+        var newPaddingStr = newPadding + 'px';
+
+        console.log(screenWidth);
+
+        mainContent.find('.slider').slick('slickSetOption', 'centerPadding', newPaddingStr, true);
     }
 };
 
 $(document).ready(function () {
     MetrixNews.InitializeActions();
+});
+
+$(window).resize(function () {
+    MetrixNews.ResizeSliders();
 });
